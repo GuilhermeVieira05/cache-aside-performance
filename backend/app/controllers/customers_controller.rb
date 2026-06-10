7fa0114c5ba6bcc4b9387962bbfd2e438103aa7f
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2026-06-09 21:18:12 -0300

    feat: CustomersController seta header X-Cache nas respostas

--- backend/app/controllers/customers_controller.rb
+++ backend/app/controllers/customers_controller.rb
@@ -2,12 +2,14 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: %i[show update destroy]
 
   def index
-    customers = CacheService.fetch("customers:all") { Customer.order(:id).to_json }
+    customers, cache_status = CacheService.fetch("customers:all") { Customer.order(:id).to_json }
+    response.headers["X-Cache"] = cache_status.to_s.upcase
     render json: customers
   end
 
   def show
-    customer = CacheService.fetch("customers:#{@customer.id}") { @customer.to_json }
+    customer, cache_status = CacheService.fetch("customers:#{@customer.id}") { @customer.to_json }
+    response.headers["X-Cache"] = cache_status.to_s.upcase
     render json: customer
   end
 
@@ -15,6 +17,7 @@ class CustomersController < ApplicationController
     customer = Customer.new(customer_params)
     if customer.save
       CacheService.invalidate("customers:all")
+      response.headers["X-Cache"] = "WRITE"
       render json: customer, status: :created
     else
       render_validation_errors(customer)
@@ -24,6 +27,7 @@ class CustomersController < ApplicationController
   def update
     if @customer.update(customer_params)
       CacheService.invalidate("customers:all", "customers:#{@customer.id}")
+      response.headers["X-Cache"] = "WRITE"
       render json: @customer
     else
       render_validation_errors(@customer)
@@ -33,6 +37,7 @@ class CustomersController < ApplicationController
   def destroy
     if @customer.destroy
       CacheService.invalidate("customers:all", "customers:#{@customer.id}")
+      response.headers["X-Cache"] = "INVALIDATED"
       head :no_content
     else
       render_validation_errors(@customer)
